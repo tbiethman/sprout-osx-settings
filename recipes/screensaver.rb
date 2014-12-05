377006af3582a01f4332e9fbea3091525f6e32a6
Tyler B <tylerb@win2k3.personal.test.com>
2014-12-05 12:09:30 -0800

    Updating domain for screensaver.

--- recipes/screensaver.rb
+++ recipes/screensaver.rb
@@ -1,14 +1,7 @@
-plist_dir = "#{node['sprout']['home']}/Library/Preferences/ByHost"
-domains = Dir["#{plist_dir}/com.apple.screensaver.*.plist"].map do |f| 
-  domain = File.basename(f).chomp('.plist')
-  "ByHost/#{domain}"
-end
-domains.each do |domain|
-  osx_defaults "set screensaver timeout" do
-    domain domain
-    key 'idleTime'
-    integer 0
-  end
+osx_defaults "set screensaver timeout" do
+  domain com.apple.screensaver
+  key 'idleTime'
+  integer 0
 end
 
 execute "set display, disk and computer sleep times" do
