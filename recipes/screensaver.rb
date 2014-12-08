c5429df2971be52c266bf4c724fda9f2b949341a
Tyler B <tylerb@win2k3.personal.test.com>
2014-12-08 08:02:03 -0800

    Commiting changes for screensaver and energy saver settings

--- recipes/screensaver.rb
+++ recipes/screensaver.rb
@@ -1,15 +1,3 @@
-osx_defaults "ask for password when screen is locked" do
-  domain 'com.apple.screensaver'
-  key 'askForPassword'
-  integer 1
-end
-
-osx_defaults "wait 60 seconds between screensaver & lock" do
-  domain 'com.apple.screensaver'
-  key 'askForPasswordDelay'
-  float 60
-end
-
 plist_dir = "#{node['sprout']['home']}/Library/Preferences/ByHost"
 domains = Dir["#{plist_dir}/com.apple.screensaver.*.plist"].map do |f| 
   domain = File.basename(f).chomp('.plist')
@@ -19,10 +7,10 @@ domains.each do |domain|
   osx_defaults "set screensaver timeout" do
     domain domain
     key 'idleTime'
-    integer 600
+    integer 0
   end
 end
 
 execute "set display, disk and computer sleep times" do
-  command "pmset -a displaysleep 20 disksleep 15 sleep 0"
+  command "pmset -a displaysleep 0 disksleep 0 sleep 0"
 end
